e356f36e4f0719280bf51c3955261a2f309bca49
Annia Sebold <anniasebold3.0@gmail.com>
2020-11-10 22:07:32 -0300

    Preenchendo campos

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -1,13 +1,17 @@
 Dado('que acesso a página de cadastro') do
     visit "http://parodify.qaninja.com.br/"
-    sleep 5 # temporário
+    click_on "EXPERIMENTE AGORA"
 end
   
 Quando('submeto o meu cadastro com:') do |table|
-    # table is a Cucumber::MultilineArgument::DataTable
-    pending # Write code here that turns the phrase above into concrete actions
+    user = table.rows_hash
+    find("input[name*=email]").set user[:email]
+    find("input[placeholder='Sua senha secreta']").set user[:senha]
+    find("input[placeholder='Confirme sua senha']").set user[:senha_confirma]
+
+    sleep 5 #temporário
 end
   
 Então('devo ser redirecionado para a área logada') do
-    pending # Write code here that turns the phrase above into concrete actions
+    
 end
