9ca0882c61da56cdb87a1074b02d92e63c30689d
Annia Sebold <anniasebold3.0@gmail.com>
2020-11-12 23:03:54 -0300

    Identando

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -1,25 +1,25 @@
 Dado("que acesso a página de cadastro") do
-    visit "http://parodify.qaninja.com.br/"
-    click_on "EXPERIMENTE AGORA"
+  visit "http://parodify.qaninja.com.br/"
+  click_on "EXPERIMENTE AGORA"
 end
-  
+
 Quando("submeto o meu cadastro com:") do |table|
-    user = table.rows_hash
+  user = table.rows_hash
 
-    delorean(user[:email])
+  delorean(user[:email])
 
-    find("input[name*=email]").set user[:email]
-    find("input[placeholder='Sua senha secreta']").set user[:senha]
-    find("input[placeholder='Confirme a senha']").set user[:confirma_senha]
+  find("input[name*=email]").set user[:email]
+  find("input[placeholder='Sua senha secreta']").set user[:senha]
+  find("input[placeholder='Confirme a senha']").set user[:confirma_senha]
 
-    click_on "Cadastrar"
+  click_on "Cadastrar"
 end
 
 Então("devo ser redirecionado para a área logada") do
-    expect(page).to have_css '.dashboard'
+  expect(page).to have_css '.dashboard'
 end
 
 Então("devo ver a mensagem: {string}") do |expect_message|
-    alert = find(".message p")
-    expect(alert.text).to eql expect_message
+  alert = find(".message p")
+  expect(alert.text).to eql expect_message
 end
